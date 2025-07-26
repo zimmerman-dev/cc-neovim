c5a47a393def44cd76b8ae87d922ee2608df92ff
John Zimmerman <140193884+zimmerman-dev@users.noreply.github.com>
2025-07-26 08:14:34 -0500

    feat: Add global transparency support within colorscheme.lua

--- lua/colorscheme/tokyonight.lua
+++ lua/colorscheme/tokyonight.lua
@@ -5,9 +5,16 @@ return {
   config = function()
     require("tokyonight").setup {
       style = "night",
-      transparent = false,
+      transparent = false, -- keep false, terminal will handle transparency
     }
-    vim.cmd([[colorscheme tokyonight]])
+    vim.cmd.colorscheme("tokyonight")
+
+    -- Clear Neovim background so terminal transparency shows through
+    vim.cmd([[
+      hi Normal guibg=NONE ctermbg=NONE
+      hi NormalNC guibg=NONE ctermbg=NONE
+      hi SignColumn guibg=NONE ctermbg=NONE
+      hi EndOfBuffer guibg=NONE ctermbg=NONE
+    ]])
   end,
 }
-
