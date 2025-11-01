082d65d5609682ffc5eb6e252534dbef6ce48d46
John Zimmerman <140193884+zimmerman-dev@users.noreply.github.com>
2025-11-01 17:50:18 -0500

    feat: added HTML, CSS, and JS support

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,16 +1,35 @@
--- lsp
+-- lua/plugins/lsp.lua
 
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.lsp.enable("clangd", {}) -- C++
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-    vim.lsp.enable("lua_ls", {   -- Lua
+    vim.lsp.enable("clangd", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable("lua_ls", {
+      capabilities = capabilities,
       settings = {
         Lua = {
-          diagnostics = { globals = { "vim" } },
+          diagnostics = {
+            globals = { "vim" },
+          },
         },
       },
+    })
+
+    vim.lsp.enable("html", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable("cssls", {
+      capabilities = capabilities,
+    })
+
+    vim.lsp.enable("tsserver", {
+      capabilities = capabilities,
     })
   end,
 }
