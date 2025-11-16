9ba9a021ca6ad1bc61aefc5d60d303cbaec2963f
John Zimmerman <zimmerman-dev@outlook.com>
2025-11-15 20:36:53 -0600

    feat: color preview highlights on hex codes

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -7,6 +7,7 @@ return {
 --  require("colorscheme.tokyonight"),
   require("colorscheme.vague"),
   require("plugins.lualine"),
+  require("plugins.colorizer"),
   require("plugins.lsp"),
   require("plugins.cmp"),
   require("plugins.autopairs"),
