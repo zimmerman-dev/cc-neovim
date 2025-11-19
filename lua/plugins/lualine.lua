9150c8add1e5b18d03d9cba6c96bea22d87bf79c
John Zimmerman <zimmerman-dev@outlook.com>
2025-11-19 17:50:12 -0600

    chore: replaced theme with tokyonight

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("lualine").setup {
       options = {
-        theme = "vague",
+        theme = "tokyonight",
         section_separators = "",
         component_separators = "",
       },
